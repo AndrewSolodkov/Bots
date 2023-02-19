38dcb0b002428a7480f1b4312c81c45a77ac0a4f
Andre <andrei240400@gmail.com>
2023-02-19 23:19:04 +0300

    BOTzaebal

--- BotPay.py
+++ BotPay.py
@@ -13,6 +13,8 @@ bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 dblya = db.DBlya()
 
+antimammoth = {} #База данных для проверки
+
 # Цена
 PRICE = types.LabeledPrice(label="Подписка на 1 месяц", amount=100 * 100)  # Цена в копейках
 
@@ -25,9 +27,10 @@ async def send_welcome(message: types.Message):
     # Создаем 2 кнопки
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    item1 = types.KeyboardButton("/Buy Купить/продлить подписку")
+    item1 = types.KeyboardButton("/buy Купить подписку")
+    item2 = types.KeyboardButton("/extension Продлить подписку")
 
-    markup.add(item1)
+    markup.add(item1, item2)
 
     # При нажатии старт сообщение, текст после "" это как раз создание клавиатуры в телеге
     await bot.send_message(message.from_user.id,
@@ -41,7 +44,7 @@ async def send_welcome(message: types.Message):
 async def buy(message: types.Message):
     await bot.send_invoice(message.chat.id,
                            title="Подписка spotify",
-                           description="Покупка/продление подписки spotify на месяц",
+                           description="Покупка подписки spotify на месяц",
                            provider_token=config.PAYMENTS_TOKEN,
                            currency="rub",
                            photo_url="https://ic.wampi.ru/2023/02/18/photo_2021-08-27_20-39-27.jpg",
@@ -51,7 +54,7 @@ async def buy(message: types.Message):
                            is_flexible=False,
                            prices=[PRICE],
                            start_parameter="one-month-subscription",
-                           payload="test-invoice-payload")
+                           payload="Buy")
 
 
 # pre chekout(проверка перед платежем (ответ должен быть в течении 10 сек)
@@ -65,12 +68,46 @@ async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
 async def successful_payment(message: types.Message):
     print("Честный выигрыши - 1xbet:")
     payment_info = message.successful_payment.to_python()
+    print(message) #Принт тут для вывода того, что происходит в консоль
     for k, v in payment_info.items():
         print(f"{k} = {v}")
-
+    antimammoth [message.from_user.id] = True #Оплатил
+    login, password = dblya.get_free_login(message.from_user.id)
     await bot.send_message(message.chat.id,
-                           f"Платеж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно")
+                           f"Платеж на сумму {message.successful_payment.total_amount // 100}"
+                           f" {message.successful_payment.currency} прошел успешно{',введите логин и пароль' if message.successful_payment.invoice_payload == 'Extension' else f', ваш логин{login} ваш пароль{password}'}")
+                                                                                                                                       #квадратные скобки и потом равно, чтобы проверить что произошло
+                                                                                                                                            #и показать полностью строку или часть
+#Продление
+@dp.message_handler(commands=['extension'])
+async def extension(message: types.Message):
+    print(message)
+    await bot.send_invoice(message.chat.id,
+                           title="Подписка spotify",
+                           description="Продление подписки spotify на месяц",
+                           provider_token=config.PAYMENTS_TOKEN,
+                           currency="rub",
+                           photo_url="https://ic.wampi.ru/2023/02/18/photo_2021-08-27_20-39-27.jpg",
+                           photo_width=416,
+                           photo_height=234,
+                           photo_size=416,
+                           is_flexible=False,
+                           prices=[PRICE],
+                           start_parameter="one-month-subscription",
+                           payload="Extension") #Вводим название того, что происходит оплата или продление
 
+
+@dp.message_handler(commands=['credentials'])   #Проверка оплатил или нет
+def get_credentials(message: types.Message):
+    try:
+        if antimammoth [message.from_user.id]:
+            arguments = message.get_args()
+            arguments_splitted = arguments.split(' ')
+            dblya.add_login(arguments_splitted[0], arguments_splitted[1], message.from_user.id)
+            bot.send_message(message.chat.id, text='Ожидайте, пока раб продлит вам подписку')
+    except:
+        bot.send_message(message.chat.id, text='Повторите попытку позже или обратитесь к администратору /help(idi nahui)')
+    antimammoth [message.from_user.id] = False
 
 # Запуск
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,31 +1,34 @@
 import sqlite3
 
-
 class DBlya:
     def __init__(self):
         self.con = sqlite3.connect("db.sqlite3")
         self.cur = self.con.cursor()
         self.cur.execute('''CREATE TABLE IF NOT EXISTS logins (
-            id serial CONSTRAINT logins_pk PRIMARY KEY,
+            id INTEGER CONSTRAINT logins_pk PRIMARY KEY,
             "email" TEXT,
             password TEXT
-        );
+        );''')
 
-        CREATE TABLE IF NOT EXISTS user_logins(
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS user_logins(
             id TEXT,
             login_id int CONSTRAINT login_fk REFERENCES logins(id)
         )
         ''')
         self.con.commit()
 
+
     def get_free_login(self, telegram_id: str) -> (str, str):
         self.cur = self.con.cursor()
         res = self.cur.execute('''
-        SELECT id, login, password 
+        SELECT logins.id, logins.email, logins.password 
         FROM logins 
-        LEFT JOIN user_logins ul ON ul.id is null''')
+        LEFT JOIN user_logins ul ON ul.login_id = logins.id 
+        WHERE ul.id is NULL''')
 
         row = res.fetchone()
+        if  row is None:
+            return "Нет", "свободных"
 
         login: str = ""
         password: str = ""
@@ -34,6 +37,7 @@ class DBlya:
         try:
             login = row[1]
             password = row[2]
+            login_id = row[0]
         except IndexError:
             pass
 
@@ -47,20 +51,14 @@ class DBlya:
     def add_login(self, login: str, password: str, telegram_id: str):
         self.cur = self.con.cursor()
         self.cur.execute('''
-            INSERT INTO logins (email, password) VALUES (?, ?) 
+            INSERT INTO need_extension (login_id) SELECT id FROM logins WHERE email = ? and password = ? 
         ''', (login, password))
-
-        lid = self.cur.lastrowid
-
-        self.cur.execute('''
-        INSERT INTO user_logins (id, login_id) VALUES (?,?)
-        ''', (telegram_id, lid))
         self.con.commit()
 
     def get_credentials(self, telegram_id: str) -> (str, str):
         self.cur = self.con.cursor()
         res = self.cur.execute('''
-            SELECT login, password 
+            SELECT email, password 
             FROM logins
             JOIN user_logins on logins.id = user_logins.login_id
             WHERE user_logins = ?''', (telegram_id, ))
